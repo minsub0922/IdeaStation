43d72d31ec00aa8eb47528afb54d09f7ea6edc87
최민섭 <minsub0922@gmail.com>
2019-08-11 12:33:42 +0900

    Initial Commit

---
+++ IdeaStationTests/IdeaStationTests.swift
@@ -0,0 +1,34 @@
+//
+//  IdeaStationTests.swift
+//  IdeaStationTests
+//
+//  Created by 최민섭 on 11/08/2019.
+//  Copyright © 2019 최민섭. All rights reserved.
+//
+
+import XCTest
+@testable import IdeaStation
+
+class IdeaStationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
