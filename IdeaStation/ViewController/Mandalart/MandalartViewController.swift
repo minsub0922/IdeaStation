c01345a4b44e70bcbba6c371def34d2d4379c7d7
정종인 <chongin12@naver.com>
2019-08-11 16:06:41 +0900

    [UI] MandalartStoryboard, MandalartViewController 추가
    기본적인 폼 추가했습니다.

---
+++ IdeaStation/ViewController/Mandalart/MandalartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MandaralartViewController.swift
+//  IdeaStation
+//
+//  Created by 정종인 on 11/08/2019.
+//  Copyright © 2019 최민섭. All rights reserved.
+//
+
+import UIKit
+
+class MandalartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
